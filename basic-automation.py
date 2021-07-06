5e452de87d6a997a3784509f4058ca86b44beaa6
Abhi-Balijepalli <AbhiBalijepalli@gmail.com>
2021-07-06 12:38:53 -0700

    just testing out scheduler built into main.py

--- basic-automation.py
+++ basic-automation.py
@@ -1,21 +1,21 @@
-import schedule
-import os
-import sys
-import os
-import time
-from main import main
+# import schedule
+# import os
+# import sys
+# import os
+# import time
+# from main import main
 
 
-class Scheduler():
-    def trigger_main(self):
-        return main()
-    def schedule_a_job():
-        schedule.every(6).hours.do(self.trigger_main)
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
+# class Scheduler():
+#     def trigger_main(self):
+#         return main()
+#     def schedule_a_job():
+#         schedule.every(6).hours.do(self.trigger_main)
+#         while True:
+#             schedule.run_pending()
+#             time.sleep(1)
 
 
-if __name__ == "__main__":
-    run = Scheduler()
-    run.scheule_a_job()
+# if __name__ == "__main__":
+#     run = Scheduler()
+#     run.scheule_a_job()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import schedule
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 from threading import Thread
 import time
@@ -7,20 +8,27 @@ from models import run_models
 from get_api import get_products
 from api import send_data
 
+class Scheduler():
+    def main(self):
+        threads = []
+        products = get_products()
+        print(products)
+        for product in products:
+            automate(product['url'], product['company_id'], product['product_id'], product['id'])
+            break
+    def schedule_a_job():
+        schedule.every(6).hours.do(self.main)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
 
-def main():
-    threads = []
-    products = get_products()
-    print(products)
-    for product in products:
-        automate(product['url'], product['company_id'], product['product_id'], product['id'])
-        break
-
-
-def automate(url, company_id, product_id, id):
-    review_data, gpt3_data, price, product_images = run_scrapping(url)
-    run_models(review_data, gpt3_data, company_id, product_id, id, price, product_images)
+    def automate(url, company_id, product_id, id):
+        review_data, gpt3_data, price, product_images = run_scrapping(url)
+        run_models(review_data, gpt3_data, company_id, product_id, id, price, product_images)
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    run = Scheduler()
+    run.scheule_a_job()
+
