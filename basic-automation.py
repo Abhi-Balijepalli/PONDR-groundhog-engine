5403db03c056135f3c485dea099d724f5cd23576
Abhi-Balijepalli <AbhiBalijepalli@gmail.com>
2021-07-06 20:18:00 -0700

    advanced scheduler

--- basic-automation.py
+++ basic-automation.py
@@ -1,12 +1,23 @@
-from crontab import CronTab
-cron = CronTab(user='username')
-job1 = cron.new(command='python3 main.py')
-job1.minute.every(30)
+# from crontab import CronTab
+# cron = CronTab(user='username')
+# job1 = cron.new(command='python3 main.py')
+# job1.minute.every(30)
 
-for item in cron:
-    print(item)
-job.clear()
+# for item in cron:
+#     print(item)
+# job.clear()
 
-for item in cron:
-    print(item)
-cron.write()
+# for item in cron:
+#     print(item)
+# cron.write()
+import os
+import sys
+from main import main
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+def some_job():
+    return main()
+
+scheduler = BlockingScheduler()
+scheduler.add_job(main, 'interval', minutes=30)
+scheduler.start()
