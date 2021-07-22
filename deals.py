5ca55946ebfec3c0aefb46ee093ac4d49f1d7687
t0mCS <tomcs333@gmail.com>
2021-07-22 16:05:00 -0700

    all threads

--- deals.py
+++ deals.py
@@ -43,8 +43,8 @@ if __name__ == "__main__":
         deals_thread.start()
         time.sleep(1)
         id = id + 1
-        if id % 15 == 0:
-            print('@@@@@@@@@@ Joining Threads @@@@@@@@@@')
-            for t in deals_threads:
-                t.join()
-            deals_thread = []
+        
+    print('@@@@@@@@@@ Joining Threads @@@@@@@@@@')
+    for t in deals_threads:
+        t.join()
+    deals_thread = []
